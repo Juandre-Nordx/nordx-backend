2f81264a7a1f51e8f3bc3b934fc1bb248770870b
Juandre Potgieter <juandre@nordx.co.za>
2026-01-27 10:12:05 +0200

    backend.auth crased

--- backend/main.py
+++ backend/main.py
@@ -4,11 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
 from dotenv import load_dotenv
 from pathlib import Path
-from backend.routes.jobcards import router as jobcards_router
-from backend.routes.admin import router as admin_router
-from backend.routes.auth import router as auth_router
-from backend.routes.users import router as users_router
-from backend.routes.health import router as health_router
+from backend.routes import auth, admin, jobcards, users, health
 import os
 
 
