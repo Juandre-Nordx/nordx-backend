37e8761bd79942eaa9ec5eee663eeb4b30c95e92
Juandre-Nordx <dev@nordx.co.za>
2026-02-01 15:15:00 +0200

    update for the super user

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Request, HTTPException, Response, Form, Depends
 from sqlalchemy.orm import Session
 from typing import List
 
